e4d46ed079fd5cb8b0c7ff8da8a0bdf12695d925
Phi-C <chenxjhit@163.com>
2025-12-27 12:16:15 +0800

    update

--- scripts/connectivity_test.sh
+++ scripts/connectivity_test.sh
@@ -22,16 +22,14 @@ install_package() {
 }
 
 connectivity_test() {
-    ping -c 1 "$TARGET_IP" &> /dev/null
-    if [ $? -eq 0 ]; then
+    if ping -c 1 "$TARGET_IP" &> /dev/null; then
         echo "Ping successful"
     else
         echo "Ping failed"
         exit 1
     fi
 
-    nc -z "$TARGET_IP" "$PORT" &> /dev/null
-    if [ $? -eq 0 ]; then
+    if nc -z "$TARGET_IP" "$PORT" &> /dev/null; then
         echo "Connection successful"
     else
         echo "Connection failed"
@@ -43,16 +41,18 @@ connectivity_test() {
 if [[ -f "/etc/os-release" ]] && grep -qi "ubuntu" /etc/os-release; then
     # apt update
     GENERAL_INSTALLER="apt install -y"
-elif [[ -f "/etc/os-release" ]] && (grep -qi "centos" /etc/os-release || grep -qi "tencentos" /et/os-release); then
+    NETCAT_PACKAGE_NAME="netcat"
+elif [[ -f "/etc/os-release" ]] && (grep -qi "centos" /etc/os-release || grep -qi "tencentos" /etc/os-release); then
     # yum update
     GENERAL_INSTALLER="yum install -y"
+    NETCAT_PACKAGE_NAME="nc"
 else
     echo "Unsupported OS"
     exit 1
 fi
 
 
-install_package "nc" "$GENERAL_INSTALLER nc"
+install_package "nc" "$GENERAL_INSTALLER $NETCAT_PACKAGE_NAME"
 install_package "ping" "$GENERAL_INSTALLER iputils-ping"
 
 connectivity_test
